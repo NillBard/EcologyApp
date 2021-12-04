aeeab4d7331b84486ce1e77704a15da8253a8205
Kirill Kumma <kirill.kumma@gmail.com>
2021-12-04 17:02:29 +0300

    add docker config

--- server/src/main.js
+++ server/src/main.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
 
+const docs = require('../docs.json')
 const { logger } = require('./core/logger')
 const { errorHandler } = require('./core/errorHandler')
 const { notFound } = require('./core/notFound')
 
 const { authRouter } = require('./auth/authRouter')
 
-const PORT = process.env.PORT || 4200
+const PORT = 4200
 const HOST = process.env.HOST || '0.0.0.0'
 
-const app = express()
-  .use(express.json())
-  .use(cors())
-  .use(logger())
-  .use(errorHandler())
-  .use(notFound())
+const app = express().use(express.json()).use(cors()).use(logger())
 
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(docs))
 app.use('/api/auth', authRouter)
+
+app.use(errorHandler())
+app.use(notFound())
 
 app.listen(PORT, HOST, () => console.log(`Server has started on port ${PORT}`))
